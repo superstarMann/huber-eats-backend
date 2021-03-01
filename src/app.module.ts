add85c62d798726e6f44ec50672825d205e06fe0
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-03-01 23:47:19 +0900

    wow

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import * as Joi from "joi";
 import {GraphQLModule} from '@nestjs/graphql';
 import { StoresModule } from './stores/stores.module';
 import { TypeOrmModule} from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+
+console.log(Joi);
 
 @Module({
   imports: [
@@ -10,8 +13,14 @@ import { ConfigModule } from '@nestjs/config';
     isGlobal: true,
     envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
     ignoreEnvFile: process.env.NODE_ENV ==='prod',
-
-
+    validationSchema: Joi.object({
+      NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+      DB_HOST: Joi.string().required(),
+      DB_PORT: Joi.string().required(),
+      DB_USERNAME: Joi.string().required(),
+      DB_PASSWORD: Joi.string().required(),
+      DB_DATABASE: Joi.string().required(),
+    })
   }),
   TypeOrmModule.forRoot({
     type: 'postgres',
