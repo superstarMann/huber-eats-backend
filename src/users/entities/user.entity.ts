1d4a6df86e4b55095446e138e8481481b64ede91
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-03-07 20:37:02 +0900

    User CRUD the end

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
 import { CoreEntity } from "src/common/entities/core.entity";
-import { BeforeInsert, Column, Entity } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity } from "typeorm";
 import * as bcrypt from "bcrypt"
 import { InternalServerErrorException } from '@nestjs/common';
 import { IsEmail, IsEnum } from "class-validator";
@@ -33,7 +33,9 @@ export class User extends CoreEntity{
     @IsEnum(UserRole)
     role: UserRole;
 
+  
     @BeforeInsert() //resolvers전에 해쉬로 바꿔버림
+    @BeforeUpdate()
     async hashPassword(): Promise<void> {
         try {
           this.password = await bcrypt.hash(this.password, 10);
--- src/users/entities/users.service.ts
+++ src/users/entities/users.service.ts
@@ -66,7 +66,14 @@ export class UsersService {
     return this.users.findOne({id});
  }
 
- async editProfile(userId: number, {email, password}: EditProfileInput){
-    this.users.update(userId, {email,password});
+ async editProfile(userId: number, {email, password}: EditProfileInput): Promise<User> {
+     const user = await this.users.findOne(userId);
+     if(email){
+         user.email = email
+     }
+     if(password){
+         user.password = password
+     }
+     return this.users.save(user);
   }
 }
