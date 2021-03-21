e8a95bc9cc6b463086ee2ba3b2209e6be120d8bd
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-03-21 22:08:56 +0900

    21.03.21

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,38 +10,41 @@ export class MailService {
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {}
 
-  private async sendEmail(
+   async sendEmail(
     subject: string,
     template: string,
     emailVars: EmailVar[],
   ) {
             const form = new FormData();
-            form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+            form.append(
+              'from',
+              `LH from Huber Eats <mailgun@${this.options.domain}>`,
+            );
             form.append('to', `wsx94503@naver.com`);
             form.append('subject', subject);
-            form.append("template", template);
+            form.append('template', template);
             emailVars.forEach(eVar => form.append(`v:${eVar.key}`, eVar.value));
-            try{
-                await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
-                method:'POST',
-                headers:{
-                    "Authorization": `Basic ${Buffer.from(`api:${this.options.apiKey}`).toString("base64")}`
-                }, 
-                body:form,
+            try {
+              await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
+                method: 'POST',
+                headers: {
+                  Authorization: `Basic ${Buffer.from(
+                    `api:${this.options.apiKey}`,
+                  ).toString('base64')}`,
+                },
+                body: form,
+              });
+            } catch (error) {
+              console.log(error);
             }
-        );
-        }catch(error){
-            console.log(error);
+          }
+          sendVerificationEmail(email: string, code: string) {
+            this.sendEmail('Verify Your Email', 'verify-email', [
+              { key: 'code', value: code },
+              { key: 'username', value: email },
+            ]);
+          }
         }
-   }
-
-   sendVerificationEmail(email: string, code: string) {
-    this.sendEmail('Verify Your Email', 'verify-email', [
-      { key: 'code', value: code },
-      { key: 'username', value: email },
-    ]);
-  }
-}
     
     
     
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import {
-  CreateAccountInput,
-  CreateAccountOutput,
-} from './dtos/create-account.dto';
+import { CreateAccountInput, CreateAccountOutput } from './dtos/create-account.dto';
 import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { JwtService } from 'src/jwt/jwt.service';
 import { EditProfileInput, EditProfileOutput } from './dtos/edit-profile.dto';
 import { Verification } from './entities/verification.entity';
-import { VerifyEmailOutput } from './dtos/verify-email.dto';
-import { UserProfileOutput } from './dtos/user-profile.dto';
 import { MailService } from 'src/mail/mail.service';
+import { UserProfileOutput } from './dtos/user-profile.dto';
+import { VerifyEmailOutput } from './dtos/verify-email.dto';
 
 @Injectable()
 export class UserService {
@@ -21,14 +18,14 @@ export class UserService {
     @InjectRepository(Verification)
     private readonly verifications: Repository<Verification>,
     private readonly jwtService: JwtService,
-    private readonly mailService: MailService
+    private readonly mailService: MailService,
   ) {}
 
   async createAccount({
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
+  }: CreateAccountInput): Promise<CreateAccountOutput> {
     try {
       const exists = await this.users.findOne({ email });
       if (exists) {
@@ -49,11 +46,7 @@ export class UserService {
     }
   }
 
-  async login({
-    email,
-    password,
-  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
-    // make a JWT and give it to the user
+  async login({ email, password }: LoginInput): Promise<LoginOutput> {
     try {
       const user = await this.users.findOne(
         { email },
@@ -65,7 +58,6 @@ export class UserService {
           error: 'User not found',
         };
       }
-
       const passwordCorrect = await user.checkPassword(password);
       if (!passwordCorrect) {
         return {
@@ -73,6 +65,7 @@ export class UserService {
           error: 'Wrong password',
         };
       }
+
       const token = this.jwtService.sign(user.id);
       return {
         ok: true,
@@ -81,35 +74,50 @@ export class UserService {
     } catch (error) {
       return {
         ok: false,
-        error,
+        error: "Can't log user in.",
       };
     }
   }
 
-  async findById(id: number): Promise<User> {
-    return this.users.findOne({ id });
+  async findById(id: number): Promise<UserProfileOutput> {
+    try {
+      const user = await this.users.findOneOrFail({ id });
+      return {
+        ok: true,
+        user,
+      };
+    } catch (error) {
+      return { ok: false, error: 'User Not Found' };
+    }
   }
 
   async editProfile(
     userId: number,
     { email, password }: EditProfileInput,
-  ): Promise<User> {
-    const user = await this.users.findOne(userId);
-    if (email) {
-      user.email = email;
-      user.verified = false;
-      const verification = await this.verifications.save(
-          this.verifications.create({ user }), 
+  ): Promise<EditProfileOutput> {
+    try {
+      const user = await this.users.findOne(userId);
+      if (email) {
+        user.email = email;
+        user.verified = false;
+        const verification = await this.verifications.save(
+          this.verifications.create({ user }),
         );
         this.mailService.sendVerificationEmail(user.email, verification.code);
+      }
+      if (password) {
+        user.password = password;
+      }
+      await this.users.save(user);
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return { ok: false, error: 'Could not update profile.' };
     }
-    if (password) {
-      user.password = password;
-    } 
-    return this.users.save(user);
-  } 
+  }
 
-  async verifyEmail(code: string): Promise<boolean> {
+  async verifyEmail(code: string): Promise<VerifyEmailOutput> {
     try {
       const verification = await this.verifications.findOne(
         { code },
@@ -119,12 +127,11 @@ export class UserService {
         verification.user.verified = true;
         await this.users.save(verification.user);
         await this.verifications.delete(verification.id);
-        return true;
+        return { ok: true };
       }
-      throw new Error();
-    } catch (e) {
-      console.log(e);
-      return false;
+      return { ok: false, error: 'Verification not found.' };
+    } catch (error) {
+      return { ok: false, error: 'Could not verify email.' };
     }
   }
 }
