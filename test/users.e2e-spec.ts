ed5cdec71fa9b7033b9beb66d9dc2b2fd85694f8
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-03-28 22:25:32 +0900

    asd
    asd

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/grqphql'
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -15,11 +24,54 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
+  afterAll(async () =>{
+    await getConnection().dropDatabase()
+    app.close();
+  })
 
-it.todo('me');
+describe('createAccount', () => {
+  const EMAIL = "Hun@wow.com";
+  it('should create account', () => {
+    return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+      query: `mutation{
+        createAccount(input:{
+          email:"${EMAIL}",
+          password:"1234",
+          role:Owner
+        }){
+          ok
+          error
+        }
+      }`,
+    }).expect(200)
+    .expect(res => {
+      expect(res.body.data.createAccount.ok).toBe(true);
+      expect(res.body.data.createAccount.error).toBe(null);
+    });
+});
+
+it.todo('should fail if account already exists', ()=>{
+  return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+    query:`mutation{
+      createAccount(input:{
+        email: "${EMAIL},
+        password: "12345",
+        role: Owner
+      }){
+        ok
+        error
+      }
+    }`,
+  }).expect(200).expect(res => {expect(res.body.data.createAccount.ok).toBe(true);
+    expect(res.body.data.createAccount.error).toBe('This is a user with that email already');
+  })
+});
+});
+
+
 it.todo('userProfile');
-it.todo('createAccount');
 it.todo('login');
-it.todo('editProfile');
+it.todo('me');
 it.todo('verifyEmail');
+it.todo('editProfile');
 });
