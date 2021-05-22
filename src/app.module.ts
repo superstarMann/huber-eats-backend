7213b4aacf0151d25e10e8bdf81ad58a4c1a5774
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-05-22 15:02:08 +0900

    awd

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,11 +31,11 @@ import { ScheduleModule } from '@nestjs/schedule';
         NODE_ENV: Joi.string()
           .valid('dev', 'production', 'test')
           .required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.string().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_NAME: Joi.string().required(),
+        DB_HOST: Joi.string(),
+        DB_PORT: Joi.string(),
+        DB_USERNAME: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_NAME: Joi.string(),
         PRIVATE_KEY: Joi.string().required(),
         MAILGUN_API_KEY: Joi.string().required(),
         MAILGUN_DOMAIN_NAME: Joi.string().required(),
@@ -44,11 +44,14 @@ import { ScheduleModule } from '@nestjs/schedule';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      ...(process.env.DATABASE_URL 
+        ? {url: process.env.DATABASE_URL,}
+        : {host: process.env.DB_HOST,
+           port: +process.env.DB_PORT,
+           username: process.env.DB_USERNAME,
+           password: process.env.DB_PASSWORD,
+           database: process.env.DB_NAME,
+        }),
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
       entities: [User, Verification, Store, Category, Dish, Order, OrderItem, Payment],
